28a1f5cc70014d53520fe50ad6623647e8ecb17a
Reuben Rodrigues <reuben.r@smarter.codes>
2021-05-13 09:06:11 -0700

    Updated Setup script

--- scripts/setup.ts
+++ scripts/setup.ts
@@ -8,23 +8,23 @@ async function main() {
   const accounts = await hre.ethers.getSigners()
   const ServiceRegistry = await hre.ethers.getContractAt(
     'ServiceRegistry',
-    deployments['1337'].ServiceRegistry.address,
+    deployments['31337'].ServiceRegistry.address,
   )
-  const GRT = await hre.ethers.getContractAt('GraphToken', deployments['1337'].GraphToken.address)
-  const Staking = await hre.ethers.getContractAt('Staking', deployments['1337'].Staking.address)
+  const GRT = await hre.ethers.getContractAt('GraphToken', deployments['31337'].GraphToken.address)
+  const Staking = await hre.ethers.getContractAt('Staking', deployments['31337'].Staking.address)
   const Controller = await hre.ethers.getContractAt(
     'Controller',
-    deployments['1337'].Controller.address,
+    deployments['31337'].Controller.address,
   )
   const EpochManager = await hre.ethers.getContractAt(
     'EpochManager',
-    deployments['1337'].EpochManager.address,
+    deployments['31337'].EpochManager.address,
   )
   const RewardsManager = await hre.ethers.getContractAt(
     'RewardsManager',
-    deployments['1337'].RewardsManager.address,
+    deployments['31337'].RewardsManager.address,
   )
-  const Curation = await hre.ethers.getContractAt('Curation', deployments['1337'].Curation.address)
+  const Curation = await hre.ethers.getContractAt('Curation', deployments['31337'].Curation.address)
 
   // unpause protocol
   await Controller.setPaused(false)
@@ -35,7 +35,7 @@ async function main() {
   // register as indexer
   const allocationTokens = hre.ethers.utils.parseEther('100000')
   await ServiceRegistry.register('http://test.com', 'ajdhg7')
-  await GRT.approve(deployments['1337'].Staking.address, allocationTokens)
+  await GRT.approve(deployments['31337'].Staking.address, allocationTokens)
   await Staking.stake(allocationTokens)
 
   // Allocate to subgraph
@@ -56,6 +56,7 @@ async function main() {
   }
   const allocationID = channelKey.address
   const subgraphDeploymentID1 = randomHexBytes()
+  const delegationAmount = hre.ethers.utils.parseEther('100000')
   await Staking.allocate(
     subgraphDeploymentID1,
     allocationTokens,
@@ -63,33 +64,53 @@ async function main() {
     hre.ethers.constants.HashZero,
     await channelKey.generateProof(indexer.address),
   )
-  await GRT.approve(deployments['1337'].Curation.address, hre.ethers.utils.parseEther('10000'))
+  await GRT.approve(deployments['31337'].Curation.address, delegationAmount)
   await Curation.mint(subgraphDeploymentID1, hre.ethers.utils.parseEther('1000'), 0)
 
   // Delegate to indexer
-  await GRT.mint(delegator.address, hre.ethers.utils.parseEther('10000'))
+  await GRT.mint(delegator.address, delegationAmount)
 
-  await GRT.connect(delegator).approve(
-    deployments['1337'].Staking.address,
-    hre.ethers.utils.parseEther('10000'),
-  )
-  await Staking.connect(delegator).delegate(indexer.address, hre.ethers.utils.parseEther('1000'))
+  await GRT.connect(delegator).approve(deployments['31337'].Staking.address, delegationAmount)
+  await Staking.connect(delegator).delegate(indexer.address, delegationAmount)
+
+  console.log('contractAccrued', await RewardsManager.getNewRewardsPerSignal())
+  console.log('rewards for allocation:', await RewardsManager.getRewards(allocationID))
+  // console.log('Delegation:', await Staking.getDelegation(indexer.address, delegator.address))
+  // console.log('Pool:', await Staking.delegationPools(indexer.address))
+  // console.log('Indexer blacne:', await GRT.balanceOf(indexer.address))
+  // console.log('Delegator blacne:', await GRT.balanceOf(delegator.address))
 
   // Progress Epochs
   await EpochManager.setEpochLength(1)
-  for (let i = 0; i < 10; i++) {
+  for (let i = 0; i < 100; i++) {
     await EpochManager.runEpoch()
   }
 
   // Close allocation
+  // console.log('contractAccrued', await RewardsManager.getNewRewardsPerSignal())
+  // console.log('rewards for allocation:', await RewardsManager.getRewards(allocationID))
+  // console.log('Delegation:', await Staking.getDelegation(indexer.address, delegator.address))
+  // console.log('Pool:', await Staking.delegationPools(indexer.address))
+
+  // await Staking.connect(delegator).collect(hre.ethers.utils.parseEther('10'), allocationID)
   await Staking.closeAllocation(allocationID, randomHexBytes())
+
+  // for (let i = 0; i < 100; i++) {
+  //   await EpochManager.runEpoch()
+  // }
+
+  // await Staking.claim(allocationID, false)
+  console.log('contractAccrued', await RewardsManager.getNewRewardsPerSignal())
+  console.log('rewards for allocation:', await RewardsManager.getRewards(allocationID))
+  // console.log('Delegation:', await Staking.getDelegation(indexer.address, delegator.address))
+  // console.log('Pool:', await Staking.delegationPools(indexer.address))
 
   // Calculate rewards
   const del = await Staking.getDelegation(indexer.address, delegator.address)
   const delShares = del.shares
   const delPool = await Staking.delegationPools(indexer.address)
   const stake = delShares.mul(delPool.tokens).div(delPool.shares)
-  const rewards = stake.sub(hre.ethers.utils.parseEther('10000'))
+  const rewards = stake.sub(delegationAmount)
   console.log('Rewards: ', rewards)
 }
 
